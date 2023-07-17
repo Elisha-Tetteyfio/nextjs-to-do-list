447f01bd47c4b24acf55fce99c02a411e6bdfbc1
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2023-07-17 08:59:02 +0000

    create template header

---
+++ src/components/task.js
@@ -0,0 +1,9 @@
+export default function Task() {
+  return (
+    <main>
+      <div className="bg-white rounded-xl h-[10vh] mx-1 shadow-neutral-500 shadow-[0_0_20px]">
+        To-do list
+      </div>
+    </main>
+  )
+}
