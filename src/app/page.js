a6a4008a5a5e1dd5bc404541fbacf73a03557e28
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2023-09-29 15:23:54 +0000

    Menu

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="min-h-screen font-mono">
-      <div className="">
+    <main className="">
+      <div className="p-5">
         Hello Elisha
       </div>
     </main>
